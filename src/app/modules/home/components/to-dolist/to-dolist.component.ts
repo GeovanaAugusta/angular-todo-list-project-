e7b8455ba9b9d161320fcf192850a0778f83f6d4
Geovana Augusta <geovanaag@hotmail.com>
2023-01-23 15:40:02 -0300

    feat: configurações iniciais

---
+++ src/app/modules/home/components/to-dolist/to-dolist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-dolist',
+  templateUrl: './to-dolist.component.html',
+  styleUrls: ['./to-dolist.component.scss']
+})
+export class ToDolistComponent {
+
+}
