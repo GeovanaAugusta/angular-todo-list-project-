4e31ad74fec19092d531733919987af968c72633
Geovana Augusta <geovanaag@hotmail.com>
2023-01-23 17:16:26 -0300

    feat: adiciona os deletes na lista de tarefas - lógica

--- src/app/modules/home/components/to-dolist/to-dolist.component.ts
+++ src/app/modules/home/components/to-dolist/to-dolist.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// Interfaces
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-to-dolist',
   templateUrl: './to-dolist.component.html',
@@ -7,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDolistComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    { task: "Minha nova task", checked: true },
+    { task: "Minha nova task2", checked: false }
+  ];
+
   constructor() {}
 
   ngOnInit(): void {
 
+  }
+
+  public deleteItemTaskList(index: number) {
+    this.taskList.splice(index, 1);
+  }
+
+  public deleteAllTaskList() {
+
+    const confirm = window.confirm("Você deseja realmente deletar tudo?")
+
+    if (confirm)
+    this.taskList = [];
   }
 
 }
