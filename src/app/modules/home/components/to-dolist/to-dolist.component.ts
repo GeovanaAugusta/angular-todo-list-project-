98b1e69061055edd5a721f455983d3a5c9207d7d
Geovana Augusta <geovanaag@hotmail.com>
2023-01-23 18:01:34 -0300

    feat: define a regra para a tarefa marcada como finalizada e validações, pra evitar espaços vazios

--- src/app/modules/home/components/to-dolist/to-dolist.component.ts
+++ src/app/modules/home/components/to-dolist/to-dolist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 // Interfaces
 import { TaskList } from '../../model/task-list';
@@ -8,17 +8,18 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './to-dolist.component.html',
   styleUrls: ['./to-dolist.component.scss']
 })
-export class ToDolistComponent implements OnInit {
+export class ToDolistComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [
-    { task: "Minha nova task", checked: true },
-    { task: "Minha nova task2", checked: false }
+    // { task: "Minha nova task", checked: true },
+    // { task: "Minha nova task2", checked: false }
   ];
 
   constructor() {}
 
-  ngOnInit(): void {
-
+  // Posicionar nas últimas posições os itens marcados como checked, ou seja, concluídos
+  ngDoCheck(): void {
+      this.taskList.sort((fist, last) => Number(fist.checked) - Number(last.checked))
   }
 
   public deleteItemTaskList(index: number) {
@@ -36,8 +37,16 @@ export class ToDolistComponent implements OnInit {
   public setEmitTaskList(task: string) {
     console.log(task);
     this.taskList.push({task: task, checked: false});
+  }
 
+  public validationInput(task: string, index: number) {
 
+    if (!task.length) {
+      const confirm = window.confirm("A tarefa está vazia, deseja deletar?")
+      
+    if (confirm)
+    this.deleteItemTaskList(index)
+  }
   }
 
 }
